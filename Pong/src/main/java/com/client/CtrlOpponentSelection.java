73d9ce8203381d0526659b55365bb3cabf465411
Christopher Carrillo <carrillocrespo17@gmail.com>
2025-11-26 16:07:37 +0100

    .

--- Pong/src/main/java/com/client/CtrlOpponentSelection.java
+++ Pong/src/main/java/com/client/CtrlOpponentSelection.java
@@ -15,6 +15,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
